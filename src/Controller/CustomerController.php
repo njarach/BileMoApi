d3d30957d85734ccb088def279a738048225ca64
njarach <101182825+njarach@users.noreply.github.com>
2025-02-15 09:57:41 +0100

    + WIP adding documentation

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -11,25 +11,27 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use OpenApi\Attributes as OA;
 
 #[Route('/api/customers')]
+/**
+ * @OA\Tag(name="Customers", description="Endpoints for managing customers")
+ */
 final class CustomerController extends AbstractController
 {
     /**
-     * @throws HttpException
      * @throws InvalidArgumentException
      */
     #[Route('', name: 'customers', methods: ['GET'])]
     public function getCustomersList(CustomerRepository $customerRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cache): JsonResponse
     {
-        $page = $request->query->get('page', 1);
-        $limit = $request->query->get('limit', 5);
+        $page = (int) $request->query->get('page', 1);
+        $limit = (int) $request->query->get('limit', 5);
 
         $currentUser = $this->getUser();
         if (!$currentUser) {
@@ -40,7 +42,7 @@ final class CustomerController extends AbstractController
 
         $jsonUsers = $cache->get($idCache, function (ItemInterface $item) use ($customerRepository, $page, $limit, $serializer, $currentUser) {
             $item->tag('customersCache');
-            $item->expiresAfter(1);
+            $item->expiresAfter(3600);
             $customers = $customerRepository->findPaginatedCustomers($currentUser, $page, $limit);
             return $serializer->serialize($customers, 'json', ['groups' => 'customer:read']);
         });
@@ -52,10 +54,9 @@ final class CustomerController extends AbstractController
      * @throws InvalidArgumentException
      */
     #[Route('/{id}', name: 'customer', methods: ['GET'])]
-    public function getUserDetail(Customer $customer, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
+    public function getCustomerDetail(Customer $customer, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
         $currentUser = $this->getUser();
-
         if (!$currentUser || $customer->getUser() !== $currentUser) {
             throw new HttpException(Response::HTTP_FORBIDDEN, 'Access denied for this resource.');
         }
@@ -71,11 +72,8 @@ final class CustomerController extends AbstractController
         return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
     }
 
-    /**
-     * @throws HttpException
-     */
     #[Route('', name: 'create_customer', methods: ['POST'])]
-    public function createCustomer(Request $request, EntityManagerInterface $em, SerializerInterface $serializer, ValidatorInterface $validator, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    public function createCustomer(Request $request, EntityManagerInterface $em, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
     {
         $currentUser = $this->getUser();
         if (!$currentUser) {
@@ -83,7 +81,7 @@ final class CustomerController extends AbstractController
         }
 
         $customer = $serializer->deserialize($request->getContent(), Customer::class, 'json');
-        $customer->setCustomer($currentUser);
+        $customer->setUser($currentUser);
 
         $errors = $validator->validate($customer);
         if (count($errors) > 0) {
@@ -93,10 +91,9 @@ final class CustomerController extends AbstractController
         $em->persist($customer);
         $em->flush();
 
-        $newUserJson = $serializer->serialize($customer, 'json', ['groups' => 'customer:read']);
-        $returnLocation = $this->generateUrl('user', ['id' => $customer->getId()]);
+        $returnLocation = $this->generateUrl('customer', ['id' => $customer->getId()]);
 
-        return new JsonResponse($newUserJson, Response::HTTP_CREATED, ['Location' => $returnLocation], true);
+        return new JsonResponse(null, Response::HTTP_CREATED, ['Location' => $returnLocation], true);
     }
 
     /**
